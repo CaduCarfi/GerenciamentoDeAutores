c4b581ce796652cfb4694b5aa43cb24d31cb2c9e
CaduCarfi <157553755+CaduCarfi@users.noreply.github.com>
2026-03-23 15:20:35 -0300

    AutoresModel

---
+++ ex4/src/main/java/Gerenciamento/de/Autores/ex4/Model/AutoresModel.java
@@ -0,0 +1,61 @@
+package Gerenciamento.de.Autores.ex4.Model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity(name = "Autores")
+@Table(name = "tb_autor")
+public class AutoresModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nomeAutor")
+    private String nome;
+    @Column(name = "nacionalidadeAutor")
+    private String nacionalidade;
+    private LocalDate anoNascimento;
+
+    public AutoresModel() {
+    }
+
+    public AutoresModel(Long id, String nome, String nacionalidade, LocalDate anoNascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.anoNascimento = anoNascimento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public LocalDate getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public void setAnoNascimento(LocalDate anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+}
